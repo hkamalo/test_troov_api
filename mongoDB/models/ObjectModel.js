46d6c6c1230eb09962d66842e557ee2ceaf06f0d
Heranca <heranca.kamalo@gmail.com>
2021-10-20 17:32:29 +0200

    add get and patch routes for objects

--- mongoDB/models/ObjectModel.js
+++ mongoDB/models/ObjectModel.js
@@ -3,18 +3,33 @@ const { mongoDbConnection } = require('../config/dbconfig.js');
 
 const ObjectModel = mongoDbConnection.model('Session', ObjectSchema);
 
-const createNewObject = async (ObjectToStore) => {
-  const newObject = await ObjectModel.create(ObjectToStore);
+const createNewObject = async (
+  userId,
+  name,
+  description,
+  lostDetails,
+  ticketCreatedAt
+) => {
+  const newObject = await ObjectModel.create({
+    userId,
+    name,
+    description,
+    lostDetails: {
+      adress: lostDetails.adress,
+      date: lostDetails.date,
+    },
+    ticketCreatedAt,
+  });
 
   return newObject;
 };
 
-const getObjectList = () => ObjectModel.find({});
+const getObjectList = (inputUserID) => ObjectModel.find({userID: inputUserID}).exec();
 
-const updateObject = (ObjectId, elementToChange) =>
-  ObjectModel.updateOne({ id: ObjectId }, elementToChange);
+const updateObject = (inputUserId, inputObjectId, elementToChange) =>
+  ObjectModel.updateOne({ id: inputObjectId, userId: inputUserId }, elementToChange).exec();
 
-const deleteObject = (ObjectId) => ObjectModel.findByIdAndDelete(ObjectId);
+const deleteObject = (ObjectId) => ObjectModel.findByIdAndDelete(ObjectId).exec();
 
 module.exports = {
   createNewObject,
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,54 @@
 const UserRouter = require('express').Router();
 const ObjectModel = require('../mongoDB/models/ObjectModel');
 
+// object creation
 UserRouter.post('/:userId/object', async (req, res) => {
+  const { userId } = req.params;
+  const { name, description, lostDetails, ticketCreatedAt } = req.body;
 
+  try {
+    const newObject = await ObjectModel.createNewObject(
+      userId,
+      name,
+      description,
+      lostDetails,
+      ticketCreatedAt
+    );
+    return res.status(200).send(`object stored :${newObject}`);
+  } catch (error) {
+    return res.status(401).send(`error in object creation : ${error}`);
+  }
+});
+
+// object update
+UserRouter.patch('/:userId/object/:objectId', async (req, res) => {
+  const { userId } = req.params;
+  const { objectId } = req.params;
+  const { name, description, lostDetails} = req.body;
+
+  try {
+    const updatedObject = await ObjectModel.updateObject(userId, objectId, {
+      name,
+      description,
+      lostDetails,
+    });
+    return res.status(200).send(`object updated :${updatedObject}`);
+  } catch (error) {
+    return res.status(401).send(`error in udpating object: ${error}`);
+  }
+});
+
+// get user's objects list
+
+UserRouter.get('/:userId/object', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const objectsList = await ObjectModel.getObjectList(userId);
+    return res.status(200).send(`list of objects :${objectsList}`);
+  } catch (error) {
+    return res.status(401).send(`error : ${error}`);
+  }
 });
 
 module.exports = UserRouter;
